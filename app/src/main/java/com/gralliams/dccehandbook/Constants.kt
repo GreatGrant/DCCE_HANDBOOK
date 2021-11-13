1d528e679b7f14826a34a53be2c36d500e7ed034
Grant Williams <greatgrant116@gmail.com>
2021-11-13 02:15:57 +0100

    Handled and received intents in  TagDisplayActivity.kt

--- app/src/main/java/com/gralliams/dccehandbook/Constants.kt
+++ app/src/main/java/com/gralliams/dccehandbook/Constants.kt
@@ -1,3 +1,4 @@
 package com.gralliams.dccehandbook
 
 const val TAG_POSITION = "TAG_POSITION"
+const val POSITION_NOT_SET =  -1
--- app/src/main/java/com/gralliams/dccehandbook/TagDisplayActivity.kt
+++ app/src/main/java/com/gralliams/dccehandbook/TagDisplayActivity.kt
@@ -2,10 +2,37 @@ package com.gralliams.dccehandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gralliams.dccehandbook.databinding.ActivityTagDisplayBinding
 
 class TagDisplayActivity : AppCompatActivity() {
+    lateinit var binding: ActivityTagDisplayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tag_display)
+        binding = ActivityTagDisplayBinding.inflate(layoutInflater)
+        setContentView( binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let { tb ->
+            tb.setHomeButtonEnabled(true)
+            tb.setHomeButtonEnabled(true)
+        }
+
+        var position = intent.getIntExtra(TAG_POSITION, POSITION_NOT_SET)
+        when(position){
+            0 -> setText(R.string.history, "History")
+            1 -> setText(R.string.philosophy, "Philosophy")
+            2 -> setText(R.string.exam_guide, "Exam Guide")
+            3 -> setText(R.string.management, "Management")
+            4 -> setText(R.string.entry_demands, "Exam Demands")
+            5 -> setText(R.string.course_duration, "Course Duration")
+            6 -> setText(R.string.graduation, "Graduation")
+            7 -> setText(R.string.grading, "Grading")
+            8 -> setText(R.string.staff_list, "Staff List")
+        }
+    }
+
+    private fun setText(stringRes: Int, toolBarTitle: String) {
+        binding.txtCollapse.text =  getString(stringRes)
+        binding.toolbar.title  = toolBarTitle
     }
 }
