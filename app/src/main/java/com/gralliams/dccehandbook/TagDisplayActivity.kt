1394bb8cbaadb6c889e719705dfd3097938aacc9
Grant Williams <greatgrant116@gmail.com>
2021-11-13 15:33:36 +0100

    Changed theme of syllabus display activity

--- app/src/main/java/com/gralliams/dccehandbook/TagDisplayActivity.kt
+++ app/src/main/java/com/gralliams/dccehandbook/TagDisplayActivity.kt
@@ -12,27 +12,37 @@ class TagDisplayActivity : AppCompatActivity() {
         setContentView( binding.root)
 
         setSupportActionBar(binding.toolbar)
+
         supportActionBar?.let { tb ->
             tb.setHomeButtonEnabled(true)
-            tb.setHomeButtonEnabled(true)
+            tb.setDisplayHomeAsUpEnabled(true)
         }
 
         var position = intent.getIntExtra(TAG_POSITION, POSITION_NOT_SET)
         when(position){
-            0 -> setText(R.string.history, "History")
-            1 -> setText(R.string.philosophy, "Philosophy")
-            2 -> setText(R.string.exam_guide, "Exam Guide")
-            3 -> setText(R.string.management, "Management")
-            4 -> setText(R.string.entry_demands, "Exam Demands")
-            5 -> setText(R.string.course_duration, "Course Duration")
-            6 -> setText(R.string.graduation, "Graduation")
-            7 -> setText(R.string.grading, "Grading")
-            8 -> setText(R.string.staff_list, "Staff List")
+            0 -> setText(R.string.history, "History", R.drawable.atbugate)
+            1 -> setText(R.string.philosophy, "Philosophy", R.drawable.atilport)
+            2 -> setText(R.string.exam_guide, "Exam Guide", R.drawable.senateport)
+            3 -> setText(R.string.management, "Management", R.drawable.undraw_grades_re_j7d6)
+            4 -> setText(R.string.entry_demands, "Exam Demands",  R.drawable.undraw_pair_programming_re_or4x)
+            5 -> setText(R.string.course_duration, "Course Duration", R.drawable.undraw_pair_programming_re_or4x)
+            6 -> setText(R.string.graduation, "Graduation",R.drawable.atbugate)
+            7 -> setText(R.string.grading, "Grading",R.drawable.atbugate)
+            8 -> setText(R.string.staff_list, "Staff List",R.drawable.atbugate)
         }
+
+
+
     }
 
-    private fun setText(stringRes: Int, toolBarTitle: String) {
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+    private fun setText(stringRes: Int, toolBarTitle: String, image:  Int) {
         binding.txtCollapse.text =  getString(stringRes)
         binding.toolbar.title  = toolBarTitle
+        binding.appBarImage.setImageResource(image)
     }
 }
