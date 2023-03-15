daba7797db34c1f1a7e13dfc3f3e747b68473f80
Great Grant <greatgrant116@gmail.com>
2023-03-15 12:59:29 +0000

    refactor duration fragment, add viewmodel, enable darkmode

--- app/src/main/java/com/gralliams/dccehandbook/ui/duration/DurationFragment.kt
+++ app/src/main/java/com/gralliams/dccehandbook/ui/duration/DurationFragment.kt
@@ -3,20 +3,15 @@ package com.gralliams.dccehandbook.ui.duration
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.*
+import android.webkit.WebSettings
 import androidx.fragment.app.Fragment
 import androidx.webkit.WebSettingsCompat
 import androidx.webkit.WebViewFeature
-import com.gralliams.dccehandbook.databinding.FragmentCurriculumBinding
 import com.gralliams.dccehandbook.databinding.FragmentDurationBinding
-import com.gralliams.dccehandbook.databinding.FragmentPhilosophyBinding
 
 class DurationFragment : Fragment() {
-
-    private var _binding: FragmentDurationBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var viewModel: DurationViewModel
+    private lateinit var binding: FragmentDurationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,38 +19,20 @@ class DurationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentDurationBinding.inflate(inflater, container, false)
+        this.binding = FragmentDurationBinding.inflate(inflater, container, false)
 
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                Configuration.UI_MODE_NIGHT_YES -> {
-                    WebSettingsCompat.setForceDark(binding.webDuration.settings,
-                        WebSettingsCompat.FORCE_DARK_ON
-                    )
-                }
-                Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                    WebSettingsCompat.setForceDark(binding.webDuration.settings,
-                        WebSettingsCompat.FORCE_DARK_OFF
-                    )
-                }
-                else -> {
-                    //
-                }
-            }
+        binding.webDuration.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK;
+
+        if(WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
+            WebSettingsCompat.setAlgorithmicDarkeningAllowed(binding.webDuration.settings, true);
         }
 
-        binding.webDuration.loadUrl("file:///android_asset/duration.html")
-        return binding.root
+        viewModel.text.observe(viewLifecycleOwner) { link ->
+            this.binding.webDuration.loadUrl(link)
+        }
+
+        return this.binding.root
     }
 
 
-
-
-
-//   m
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
--- app/src/main/java/com/gralliams/dccehandbook/ui/duration/DurationViewModel.kt
+++ app/src/main/java/com/gralliams/dccehandbook/ui/duration/DurationViewModel.kt
@@ -1,12 +1,12 @@
-package com.gralliams.dccehandbook.ui.curriculum
+package com.gralliams.dccehandbook.ui.duration
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CurriculumViewModel: ViewModel() {
+class DurationViewModel: ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "file:///android_asset/curriculum.html"
+        value = "file:///android_asset/duration.html"
     }
     val text: LiveData<String> = _text
 
