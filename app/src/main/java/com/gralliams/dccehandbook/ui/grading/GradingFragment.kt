c3f90017aaff1a8b078bfb421dc8120d420b9338
Great Grant <greatgrant116@gmail.com>
2023-03-15 13:16:33 +0000

    bug fix: initialize viewmodel in grading fragment

--- app/src/main/java/com/gralliams/dccehandbook/ui/grading/GradingFragment.kt
+++ app/src/main/java/com/gralliams/dccehandbook/ui/grading/GradingFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import android.webkit.WebSettings
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.webkit.WebSettingsCompat
 import androidx.webkit.WebViewFeature
 import com.gralliams.dccehandbook.databinding.FragmentGradingBinding
@@ -19,6 +20,7 @@ class GradingFragment : Fragment() {
     ): View? {
 
         this.binding = FragmentGradingBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[GradingViewModel::class.java]
 
         binding.webGrading.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK;
         if(WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
