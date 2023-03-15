159bb4d4e9c3654480422b5cd3a256861dd4e372
Great Grant <greatgrant116@gmail.com>
2023-03-15 12:47:41 +0000

    admission fragment, enable darkmode

--- app/src/main/java/com/gralliams/dccehandbook/ui/admission/AdmissionFragment.kt
+++ app/src/main/java/com/gralliams/dccehandbook/ui/admission/AdmissionFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,11 +16,7 @@ import com.gralliams.dccehandbook.databinding.FragmentAdmissionBinding
 class AdmissionFragment : Fragment() {
 
     private lateinit var mAdmisionViewModel: AdmisionViewModel
-    private var _binding: FragmentAdmissionBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentAdmissionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,35 +28,20 @@ class AdmissionFragment : Fragment() {
         mAdmisionViewModel =
             ViewModelProvider(this)[AdmisionViewModel::class.java]
 
-        _binding = FragmentAdmissionBinding.inflate(inflater, container, false)
+        this.binding = FragmentAdmissionBinding.inflate(inflater, container, false)
+        val root: View = this.binding.root
 
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                Configuration.UI_MODE_NIGHT_YES -> {
-                    WebSettingsCompat.setForceDark(binding.webAdmision.settings,
-                        WebSettingsCompat.FORCE_DARK_ON
-                    )
-                }
-                Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                    WebSettingsCompat.setForceDark(binding.webAdmision.settings,
-                        WebSettingsCompat.FORCE_DARK_OFF
-                    )
-                }
-                else -> {
-                    //
-                }
-            }
+        binding.webAdmision.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK;
+
+        if(WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
+            WebSettingsCompat.setAlgorithmicDarkeningAllowed(binding.webAdmision.settings, true);
         }
-        val root: View = binding.root
+
 
         mAdmisionViewModel.text.observe(viewLifecycleOwner, Observer {
-            binding.webAdmision.loadUrl(it)
+            this.binding.webAdmision.loadUrl(it)
         })
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
